921f5b81d36c38ee196f5f649b0573e9c5a3ab4b
JadeB01 <bar22102@byu.edu>
2025-09-20 12:33:34 -0600

    Finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        //Converting types: Convert a value of a variable to a different type: int.Parse(): create an integer from the digits stored in a string.
+        //Examples: String valueInText = "42";
+        //          int number = int.Parse(valueInText);
+        int percentage = int.Parse(answer);
+
+        string letter = "";
+
+        if (percentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("Good job! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass.")
+        }
     }
 }
